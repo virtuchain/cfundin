50564ec25ceafa747740c1e2a27185d298168b05
CoolWhite <2633248411@qq.com>
2019-10-26 22:20:57 +0800

    加入了阿里oss包，发起众筹的控制层操作

--- src/main/java/com/xq/crowd_funding/webcontorller/ClassificationController.java
+++ src/main/java/com/xq/crowd_funding/webcontorller/ClassificationController.java
@@ -3,12 +3,10 @@ package com.xq.crowd_funding.webcontorller;/*
 */
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 分类模块
  */
 @RequestMapping("classification")
-@ResponseBody
 public class ClassificationController {
 }
