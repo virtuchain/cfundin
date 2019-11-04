c113cc5bd9aeabcead743e6c3a92fc49d49d80cd
Mzh <18827542206@139.com>
2019-11-04 15:49:22 +0800

    sss

--- src/main/java/com/xq/crowd_funding/manager/dao/TRoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.xq.crowd_funding.manager.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import java.util.List;
-
-
-public interface TRoleDao {
-}
