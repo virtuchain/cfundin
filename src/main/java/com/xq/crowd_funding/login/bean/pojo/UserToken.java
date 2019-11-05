b8411426278d3fe78b2f69a69aff4585591d83d7
李世明 <2076746315@qq.com>
2019-11-05 19:10:19 +0800

    login的提交

---
+++ src/main/java/com/xq/crowd_funding/login/bean/pojo/UserToken.java
@@ -0,0 +1,11 @@
+package com.xq.crowd_funding.login.bean.pojo;
+
+import lombok.Data;
+
+@Data
+public class UserToken {
+
+    private String loginacct;
+    private String userpswd;
+    private String usertoken;
+}
